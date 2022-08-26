c1f6ce6525203da9cf77b380d7e6229654d66d87
yasharalee <77319904+yasharalee@users.noreply.github.com>
2022-08-25 22:25:30 -0700

    Project initiated in local repo

--- src/practices/ZeroFront.java
+++ src/practices/ZeroFront.java
@@ -1,7 +1,10 @@
 package practices;
 
 /*
-Return an array that contains the exact same numbers as the given array, but rearranged so that all the zeros are grouped at the start of the array. The order of the non-zero numbers does not matter. So {1, 0, 0, 1} becomes {0 ,0, 1, 1}. You may modify and return the given array or make a new array.
+Return an array that contains the exact same numbers as the given array, but
+rearranged so that all the zeros are grouped at the start of the array. The order of
+the non-zero numbers does not matter. So {1, 0, 0, 1} becomes {0 ,0, 1, 1}. You may
+modify and return the given array or make a new array.
 
 
 zeroFront([1, 0, 0, 1]) → [0, 0, 1, 1]
