3c9f25483579a05e906627a62afaa6305b93d62f
yasharalee <77319904+yasharalee@users.noreply.github.com>
2022-08-28 16:44:45 -0700

    ReverseWord Added

---
+++ src/practices/ReverseWord.java
@@ -0,0 +1,21 @@
+package practices;
+
+/*
+    [it is a bit harder than what it looks]
+
+    Complete the function that accepts a string parameter, and reverses each word in the string.
+    Note: All spaces in the string should be retained.
+
+    "Hello world" -->  "olleH dlrow"
+    "a b c d" --> "a b c d"
+    "    " --> "    "
+    "a" --> "a"
+    "  b  fd" --> "  b  df"
+
+ */
+
+public class ReverseWord {
+    public static void main(String[] args) {
+        // ...
+    }
+}
