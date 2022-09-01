63e06e127851d5ef31ac7e05aca2e60159f9a027
yasharalee <77319904+yasharalee@users.noreply.github.com>
2022-08-31 22:41:09 -0700

    Frog Prob added

---
+++ src/practices/RiverCrossingProblem.java
@@ -0,0 +1,19 @@
+package practices;
+/*
+        [ AMAZON INTERVIEW QUESTION ]
+
+    Imagine there is a river which is 11 feet's wide,
+    There are some stones among the river which each one is 1 foot apart from the other one,
+    and extended from one side of the river to the other side.
+    A frog wants to cross the river, and it can either jump 1 stone or 2 stones at a time,
+    calculate how many ways are there that frog can cross the river.
+
+    Note, it can be solved even without using recursion.
+
+ */
+
+public class RiverCrossingProblem {
+    public static void main(String[] args) {
+         // coming soon ... :D
+    }
+}
