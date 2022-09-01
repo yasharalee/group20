92f84f565717d81a740c9e5b58e30ee43abd2be0
yasharalee <77319904+yasharalee@users.noreply.github.com>
2022-08-31 22:50:24 -0700

    NotReplace Solution

--- src/practices/NotReplace.java
+++ src/practices/NotReplace.java
@@ -12,8 +12,34 @@ notReplace("is-is") → "is not-is not"
 notReplace("This is right") → "This is not right"
  */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class NotReplace {
     public static void main(String[] args) {
+        System.out.println(notReplace("is test"));
+        System.out.println(notReplace("is-is"));
+        System.out.println(notReplace("This is right"));
 
+    }
+
+    public static String notReplace(String str) {
+        String res = "";
+        String[] arr = str.split(" ");
+        ArrayList<String> temp = new ArrayList<>(Arrays.asList(str.split(" ")));
+
+        for (String each : temp) { // start
+            for (int i = 0; i < each.length(); i++) {
+                char let = each.charAt(i);
+                if ((!Character.isLetter(let) && each.contains("is")) || each.equals("is")) {
+                    temp.set(temp.indexOf(each), each.replace("is", "is not"));
+                    break;
+                }
+            }
+        } // end
+        for (String e : temp) {
+            res += e + " ";
+        }
+        return res.trim();
     }
 }
